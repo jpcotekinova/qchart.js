2b100ebf7c62f3d2a27f01a481e0acc7484f2547
Fargier Sylvain <fargier.sylvain@free.fr>
2015-01-18 22:58:50 +0100

    Update QChart.qml rendering process
     - Now tooltips can be displayed and everything seems to work properly

--- QChart.qml
+++ QChart.qml
@@ -29,7 +29,9 @@ Canvas {
   property alias chartAnimationEasing: chartAnimator.easing.type;
   property alias chartAnimationDuration: chartAnimator.duration;
   property   int chartAnimationProgress: 0;
-  property   var chartOptions: ({})
+  property   var chartOptions: ({});
+
+  property   var events: ({}); /* internal use */
 
 // /////////////////////////////////////////////////////////////////
 // Callbacks
@@ -40,28 +42,27 @@ Canvas {
 
           switch(chartType) {
           case Charts.ChartType.BAR:
-              chart = new Charts.Chart(canvas, canvas.getContext("2d")).Bar(chartData, chartOptions);
+              chart = new Charts.Chart(canvas.getContext("2d")).Bar(chartData, chartOptions);
               break;
           case Charts.ChartType.DOUGHNUT:
-              chart = new Charts.Chart(canvas, canvas.getContext("2d")).Doughnut(chartData, chartOptions);
+              chart = new Charts.Chart(canvas.getContext("2d")).Doughnut(chartData, chartOptions);
               break;
           case Charts.ChartType.LINE:
-              chart = new Charts.Chart(canvas, canvas.getContext("2d")).Line(chartData, chartOptions);
+                          console.log("here")
+              chart = new Charts.Chart(canvas.getContext("2d")).Line(chartData, chartOptions);
               break;
           case Charts.ChartType.PIE:
-              chart = new Charts.Chart(canvas, canvas.getContext("2d")).Pie(chartData, chartOptions);
+              chart = new Charts.Chart(canvas.getContext("2d")).Pie(chartData, chartOptions);
               break;
           case Charts.ChartType.POLAR:
-              chart = new Charts.Chart(canvas, canvas.getContext("2d")).PolarArea(chartData, chartOptions);
+              chart = new Charts.Chart(canvas.getContext("2d")).PolarArea(chartData, chartOptions);
               break;
           case Charts.ChartType.RADAR:
-              chart = new Charts.Chart(canvas, canvas.getContext("2d")).Radar(chartData, chartOptions);
+              chart = new Charts.Chart(canvas.getContext("2d")).Radar(chartData, chartOptions);
               break;
           default:
               console.log('Chart type should be specified.');
           }
-
-          chart.init();
 
           if (chartAnimated)
               chartAnimator.start();
@@ -69,7 +70,25 @@ Canvas {
               chartAnimationProgress = 100;
       }
 
-      chart.draw(chartAnimationProgress/100);
+      if (!chartAnimator.running && events.mouseout) {
+          var cb = eventCallback("mouseout")
+          if (cb)
+              cb(events.mouseout);
+          events.mouseout = null;
+          events.mousemove = null;
+      }
+      else if (!chartAnimator.running && events.mousemove) {
+          var cb = eventCallback("mousemove")
+          if (cb)
+              cb(events.mousemove);
+          events.mousemove = null;
+          chartAnimationProgress = 100;
+      }
+      else {
+          // On some charts 0 draws the full picture, negative values is always buggy
+          chart.draw((chartAnimationProgress > 0) ?
+                  chartAnimationProgress/100 : 0.0001);
+      }
   }
 
   onHeightChanged: {
@@ -93,9 +112,54 @@ Canvas {
       chartAnimator.start();
   }
 
+  function addEventListener(eventType, method) {
+      /* empty function that must exist on the canvas (called by Chart.js) */
+  }
+
+  function getBoundingClientRect() {
+    return {
+      left: 0,
+       top: 0,
+     right: canvasWindow.width,
+    bottom: canvasWindow.height
+    }
+  }
+
+  function eventCallback(name) {
+      if (chart && chart.events) {
+          return chart.events[name];
+      }
+      return null;
+  }
 // /////////////////////////////////////////////////////////////////
 // Internals
 // /////////////////////////////////////////////////////////////////
+  MouseArea {
+    id: mouseArea;
+    anchors.fill: parent;
+    hoverEnabled: true;
+    onPositionChanged: {
+      if (parent.eventCallback("mousemove")) {
+        parent.events.mousemove = {
+                type: 'mousemove',
+             clientX: mouse.x,
+             clientY: mouse.y,
+          srcElement: parent
+        };
+        parent.requestPaint();
+      }
+    }
+
+    onExited: {
+      if (parent.eventCallback("mouseout")) {
+        parent.events.mousemove = {
+                type: 'mouseout',
+          srcElement: parent
+        };
+        parent.requestPaint();
+      }
+    }
+  }
 
   PropertyAnimation {
              id: chartAnimator;
--- QChartGallery.js
+++ QChartGallery.js
@@ -23,12 +23,16 @@ var ChartLineData = {
              strokeColor: "rgba(220,220,220,1)",
               pointColor: "rgba(220,220,220,1)",
         pointStrokeColor: "#ffffff",
+      pointHighlightFill: "#ffffff",
+    pointHighlightStroke: "rgba(220,220,220,1)",
                     data: [65,59,90,81,56,55,40]
     }, {
                fillColor: "rgba(151,187,205,0.5)",
              strokeColor: "rgba(151,187,205,1)",
               pointColor: "rgba(151,187,205,1)",
         pointStrokeColor: "#ffffff",
+      pointHighlightFill: "#ffffff",
+    pointHighlightStroke: "rgba(151,187,205,1)",
                     data: [28,48,40,19,96,27,100]
     }]
 }
@@ -37,25 +41,38 @@ var ChartLineData = {
 // Polar Chart Data Sample
 // /////////////////////////////////////////////////////////////////
 
-var ChartPolarData = [{
-    value: 30,
-    color: "#D97041"
- }, {
-    value: 90,
-    color: "#C7604C"
- }, {
-    value: 24,
-    color: "#21323D"
- }, {
-    value: 58,
-    color: "#9D9B7F"
- }, {
-    value: 82,
-    color: "#7D4F6D"
- }, {
-    value: 8,
-    color: "#584A5E"
-}]
+var ChartPolarData = [
+    {
+        value: 200,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    },
+    {
+        value: 40,
+        color: "#949FB1",
+        highlight: "#A8B3C5",
+        label: "Grey"
+    },
+    {
+        value: 120,
+        color: "#4D5360",
+        highlight: "#616774",
+        label: "Dark Grey"
+    }
+]
 
 // /////////////////////////////////////////////////////////////////
 // Radar Chart Data Sample
@@ -68,12 +85,16 @@ var ChartRadarData = {
              strokeColor: "rgba(220,220,220,1)",
               pointColor: "rgba(220,220,220,1)",
         pointStrokeColor: "#fff",
+    pointHighlightStroke: "rgba(220,220,220,1)",
+      pointHighlightFill: "#fff",
                     data: [65,59,90,81,56,55,40]
     }, {
                fillColor: "rgba(151,187,205,0.5)",
              strokeColor: "rgba(151,187,205,1)",
               pointColor: "rgba(151,187,205,1)",
         pointStrokeColor: "#fff",
+    pointHighlightStroke: "rgba(151,187,205,1)",
+      pointHighlightFill: "#fff",
                     data: [28,48,40,19,96,27,100]
     }]
 }
@@ -82,37 +103,51 @@ var ChartRadarData = {
 // Pie Chart Data Sample
 // /////////////////////////////////////////////////////////////////
 
-var ChartPieData = [{
-    value: 30,
-    color: "#F38630"
- }, {
-    value: 50,
-    color: "#E0E4CC"
- }, {
-    value: 100,
-    color: "#69D2E7"
-}]
+var ChartPieData = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
 
 // /////////////////////////////////////////////////////////////////
 // Doughnut Chart Data Sample
 // /////////////////////////////////////////////////////////////////
 
-var ChartDoughnutData = [{
-    value: 30,
-    color: "#F7464A"
- }, {
-    value: 50,
-    color: "#E2EAE9"
- }, {
-    value: 100,
-    color: "#D4CCC5"
- }, {
-    value: 40,
-    color: "#949FB1"
- }, {
-    value: 120,
-    color: "#4D5360"
-}]
+var ChartDoughnutData = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
 
 // /////////////////////////////////////////////////////////////////
 // Bar Chart Data Sample
@@ -123,10 +158,14 @@ var ChartBarData = {
     datasets: [{
           fillColor: "rgba(220,220,220,0.5)",
         strokeColor: "rgba(220,220,220,1)",
+      highlightFill: "rgba(220,220,220,0.75)",
+    highlightStroke: "rgba(220,220,220,1)",
                data: [65,59,90,81,56,55,40]
     }, {
           fillColor: "rgba(151,187,205,0.5)",
         strokeColor: "rgba(151,187,205,1)",
+      highlightFill: "rgba(151,187,205,0.75)",
+    highlightStroke: "rgba(151,187,205,1)",
                data: [28,48,40,19,96,27,100]
     }]
 }
